3657dbc752e291e4bb2309b950062df5c4864dcd
cornprincess <xiaocorn96@gmail.com>
2020-07-18 23:07:46 +0800

    auto commit

---
+++ algorithms/java/ch07ConstructBinaryTree/TreeNode.java
@@ -0,0 +1,16 @@
+package ch07ConstructBinaryTree;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhoutianbin
+ * Date: 2020-07-18
+ * Time: 23:06
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
