e9c0cd79fb22e96924dc7ab5169bc93090087b49
周天斌 <zhoutianbin@1LZHOUTIANBIN.local>
2021-02-09 14:06:05 +0800

    auto commit

--- algorithms/java/ch07ConstructBinaryTree/ConstructBinaryTree.java
+++ algorithms/java/ch07ConstructBinaryTree/ConstructBinaryTree.java
@@ -9,8 +9,8 @@
  * 
  * 例如，给出
  * 
- * 前序遍历 preorder = [3,9,20,15,7]
- * 中序遍历 inorder = [9,3,15,20,7]
+ * 前序遍历 preorder = [3,| 9,| 20,15,7]
+ * 中序遍历 inorder = [9,| 3, |15,20,7]
  * 
  * 返回如下的二叉树：
  * 
@@ -30,16 +30,54 @@
 
 package ch07ConstructBinaryTree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConstructBinaryTree {
+    private Map<Integer, Integer> indexMap;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        if (preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0) {
+            return null;
+        }
+        int n = preorder.length;
+        // inorder root hashmap
+        indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+
+        return buildTree0(preorder, inorder,0, n - 1, 0, n - 1);
+    }
+
+    private TreeNode buildTree0(int[] preordr, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        // 终止条件
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+
+        // 前序遍历的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 中序遍历中定位根节点
+        int inorder_root = indexMap.get(preordr[preorder_root]);
+
+        // 建立根节点
+        TreeNode root = new TreeNode(preordr[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归构造左子树并链接到根节点
+        root.left = buildTree0(preordr, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归构造右子树，并连接到根节点
+        root.right = buildTree0(preordr, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+
+        return root;
     }
 
     public void front_order_print(TreeNode root) {
         if (root == null) {
             return;
         }
-        System.out.println(root.val);
+        System.out.print(root.val);
         front_order_print(root.left);
         front_order_print(root.right);
     }
@@ -49,7 +87,7 @@ public class ConstructBinaryTree {
             return;
         }
         middle_order_print(root.left);
-        System.out.println(root.val);
+        System.out.print(root.val);
         middle_order_print(root.right);
     }
 
@@ -59,7 +97,7 @@ public class ConstructBinaryTree {
         }
         back_order_print(root.left);
         back_order_print(root.right);
-        System.out.println(root.val);
+        System.out.print(root.val);
     }
 
     public TreeNode initTree() {
